b2ca44612bbfb71bc4dd862fd1ec64a0fc553d81
GearGurl <linaglizer233@gmail.com>
2023-10-18 00:37:48 +0300

    fixed add to inv and added remove from inv

--- Main/player.py
+++ Main/player.py
@@ -6,13 +6,14 @@ the idea for the inventory is as follows:
   with the player and player stashes (idk if they will be added or not) to be always saved in the save of the game
   and called for when needed (when crafting or when opening up the inventory)
   npc inventories are another thing, I don't want to have them loaded on the RAM for the entire game so a similar
-  thing of loading on interaction will be made, I think for example shops will have a loot table to get what they
+  thing of loading on interaction will be made, I think for example, shops will have a loot table to get what they
   sell and after its loaded they get a seed of sorts that will be placed in a JSON file with their npc ID
   so when you go to interact with them, they can decipher the seed and load the inventory meaning you need to keep
   track of fewer things in the save file, that should be simple.
-  a similar system can be used to the players inventory whe I think about it, more heavy on the cpu but less ram usage
-  I mean come on how hard would it be to decipher the first letter of an item and a number lol
-  as per saving prices that will come later, today I focus on the player and try to make a general inventory class
+  a similar system can be used to the player inventory when I think about it. more heavy on the cpu but less ram usage
+  I mean comes on how hard would it be to decipher the first letter of an item and a number lol.
+  as per saving prices that will come later.
+  today I focus on the player and try to make a general inventory class
 """
 import re
 
@@ -57,16 +58,36 @@ class Inventory:
 
     def add_item(self, item: Item, amount):
         # find if already exists in inventory and add if it is there
+        found = False
         x = re.findall(r"(\d+[a-zA-z]+)", self.seed)
-        for item in x:
-            inv_name = re.split(r"([a-zA-Z]+)", item)
+        for each_item in x:
+            inv_name = re.split(r"([a-zA-Z]+)", each_item)
             if inv_name[1] == item.inventory_name:
+                found = True
                 number = int(inv_name[0]) + amount
                 change = str(number) + inv_name[1]
-                self.seed = re.sub(item, change, self.seed)
+                self.seed = re.sub(each_item, change, self.seed)
                 break
-        # if not lets just append it to the inventory string
-        self.seed = self.seed + str(amount) + item.inventory_name
+        # if not, lets append it to the inventory string
+        if not found:
+            self.seed = self.seed + str(amount) + item.inventory_name
+
+    def remove_item(self, item: Item, amount):
+        # look for item in inventory and remove if you have enough to remove if not enough or none exist then return
+        # None
+        x = re.findall(r"(\d+[a-zA-z]+)", self.seed)
+        for each_item in x:
+            inv_name = re.split(r"([a-zA-Z]+)", each_item)
+            if inv_name[1] == item.inventory_name:
+                if int(inv_name[0]) >= amount:
+                    number = int(inv_name[0]) - amount
+                else:
+                    return None  # return None if you don't have enough
+                change = str(number) + inv_name[1]
+                self.seed = re.sub(each_item, change, self.seed)
+                pass
+        return None  # if it gets to this point, that means we looped through the entire inventory and didn't find it
+
 
 class Player:
     def __init__(self, species, name, location, affinity, gender, extra_passives: list, Class):
@@ -96,3 +117,11 @@ a = Species("cat person", 100, 50, 10, 5, 2, [None], 80, 5)
 c = Class("knight", 2, 3, 0, 4, 0, 5, ["for the glory"])
 
 b = Player(a, "Lina", [0, 0], "idk some magic shit", "female", ["cute", "funny"], c)
+
+invn = Inventory("34io")
+itm = Item("iron ore", "io")
+invn.add_item(itm, 10)
+invn.remove_item(itm, 50)
+if invn.remove_item(itm, 50) is None:
+    print("failed to remove item you don't have enough!")
+print(invn.seed)
