c7ca9ee70f94b639f2120491105e653467a9c9d3
GearGurl <linaglizer233@gmail.com>
2023-10-24 15:12:07 +0300

    added testing file in outside scope
    its to deal with vs code being shitty and not being able to use relative imports inside a file trying to look outwards

--- Main/materials/alloy/alloy.py
+++ Main/materials/alloy/alloy.py
@@ -5,16 +5,17 @@ print("hello")
 
 class Alloy_Material(refined_materials):
     def validate_rarity(rarity):
-        #check if rarity is valid
-        if rarity != RARITIES():
-            return "Error: Rarity Not valid"
-        else:
-            return rarity
+        pass
+ #       #check if rarity is valid
+ #       if rarity != RARITIES():
+ #           return "Error: Rarity Not valid"
+ #       else:
+ #           return rarity
     
 
-    def __init__(self, name, rarity: validate_rarity(str()), description, affinity, alloy_passive, alloy_lvl = 1):
+    def __init__(self, name, description, affinity, alloy_passive, alloy_lvl = 1):
         self.name = name
-        self.rarity = rarity
+        #self.rarity = rarity
         self.description = description
         self.affinity = affinity
         self.alloy_passive = alloy_passive
@@ -27,8 +28,3 @@ class Alloy_Material(refined_materials):
         pass
 
 
-fae_gold = Alloy_Material("Fae-Gold", "rare", "A shimmering alloy created by combining Fae Ore and Gold. It harnesses the enchanting qualities of Fae Ore and the precious nature of gold, resulting in a material that enhances magical abilities, offers protection against magical attacks, and radiates a captivating aura.",
-    "Natural", "Wish", 2)
-
-
-isinstance(fae_gold, Alloy_Material)
---
+++ test.py
@@ -0,0 +1,7 @@
+import Main.materials.alloy.alloy as al
+
+fae_gold = al.Alloy_Material("Fae-Gold", "A shimmering alloy created by combining Fae Ore and Gold. It harnesses the enchanting qualities of Fae Ore and the precious nature of gold, resulting in a material that enhances magical abilities, offers protection against magical attacks, and radiates a captivating aura.",
+    "Natural", "Wish", 2)
+
+
+isinstance(fae_gold, al.Alloy_Material)
