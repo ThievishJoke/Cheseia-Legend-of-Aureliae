d51ae5af80e9c3d69eeeb1e0b9c00a34259a3cc9
BaLLducked <westth2003@gmail.com>
2023-11-19 05:30:12 -0500

    Idk im just commiting random stuff :woman_shrugging:

---
+++ Main/apparel/armor/armor_classing.py
@@ -0,0 +1,13 @@
+import materials.material
+import rarity.item_rarities
+
+def resistance_values(physical_resistance, magic_resistance):
+    physical_resistance = physical_resistance
+    magic_resistance = magic_resistance
+    return resistance_values
+
+class helmet:
+    def __init__(self, name, material, armor_weight_class, armor_skill_req, armor_perks, is_craftable, resistance_values = (0,0)):
+        self.name = name
+        self.material = material
+        #class details
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import Main.materials.alloy.alloy as al
+import Main.materials.alloy.alloying as al
 
 fae_gold = al.Alloy_Material("Fae-Gold", "A shimmering alloy created by combining Fae Ore and Gold. It harnesses the enchanting qualities of Fae Ore and the precious nature of gold, resulting in a material that enhances magical abilities, offers protection against magical attacks, and radiates a captivating aura.",
     "Natural", "Wish", 2)
